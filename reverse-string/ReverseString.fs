12ce6db8c94fd96160a18ad187902f6e6e787d43
Michael Cooper <coop87@gmail.com>
2019-03-03 14:08:58 -0500

    Complete Gigasecond, Reverse String, Rna Transcription

---
+++ reverse-string/ReverseString.fs
@@ -0,0 +1,7 @@
+﻿module ReverseString
+
+open System
+
+let reverse (input: string): string =
+    let append (c:Char) (str:string) = str + (string c)
+    Seq.foldBack append input ""
---
+++ rna-transcription/RnaTranscription.fs
@@ -0,0 +1,10 @@
+﻿module RnaTranscription
+
+let toRna (dna: string): string =
+    dna
+    |> String.collect (function
+        | 'C' -> "G"
+        | 'G' -> "C"
+        | 'T' -> "A"
+        | 'A' -> "U"
+        | _ -> "")
