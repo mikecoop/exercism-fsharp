0727e131a010481144e5256aae77d66a94480b64
Michael Cooper <coop87@gmail.com>
2019-03-03 11:04:17 -0500

    Finish Binary Search Tree and Difference of Squares

---
+++ binary-search-tree/BinarySearchTree.fs
@@ -0,0 +1,32 @@
+﻿module BinarySearchTree
+
+type BinaryTree = { Value: int; Left: BinaryTree option; Right: BinaryTree option }
+
+let left node = node.Left
+
+let right node = node.Right
+
+let data node = node.Value
+
+let rec insert (tree:BinaryTree option) (value:int) =
+    match tree with
+    | Some t ->
+        if value <= t.Value then
+            { t with Left = Some (insert t.Left value) }
+        else
+            { t with Right = Some (insert t.Right value) }
+    | None ->
+        { Value = value; Left = None; Right = None }
+
+let create items =
+    let tree = items |> List.fold (fun acc value -> Some (insert acc value)) None
+    match tree with
+    | Some t -> t
+    | None -> failwith "No values in tree"
+
+let sortedData (node:BinaryTree) =
+    let rec traverse tree =
+        match tree with
+        | None -> [ ]
+        | Some t -> (traverse t.Left) @ [ t.Value ] @ (traverse t.Right)
+    traverse (Some node)
---
+++ difference-of-squares/DifferenceOfSquares.fs
@@ -0,0 +1,15 @@
+﻿module DifferenceOfSquares
+
+let squareOfSum (number: int): int =
+    let sum =
+        [ 1 .. number ]
+        |> List.reduce (+)
+    pown sum 2
+
+let sumOfSquares (number: int): int =
+    [ 1 .. number ]
+    |> List.map (fun x -> x * x)
+    |> List.reduce (+)
+
+let differenceOfSquares (number: int): int =
+    (squareOfSum number) - (sumOfSquares number)
