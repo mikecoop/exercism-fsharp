c21c27c1ede160650fa1245e85206efa48102bba
Michael Cooper <coop87@gmail.com>
2019-03-10 11:19:14 -0400

    Finish Acronym and Matrix

---
+++ acronym/Acronym.fs
@@ -0,0 +1,6 @@
+﻿module Acronym
+
+let abbreviate (phrase:string) =
+    phrase.ToLower()
+    |> System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase
+    |> String.filter System.Char.IsUpper
---
+++ matrix/Matrix.fs
@@ -0,0 +1,18 @@
+﻿module Matrix
+
+let parseMatrix (matrix:string) =
+    matrix.Split('\n')
+    |> List.ofArray
+    |> List.map (fun s-> 
+        s.Split(' ')
+        |> List.ofArray
+        |> List.map System.Int32.Parse)
+
+let row index matrix =
+    let parsed = parseMatrix matrix
+    parsed.[index]
+
+let column index matrix =
+    let parsed = parseMatrix matrix
+    parsed
+    |> List.map (fun list -> list.[index])
