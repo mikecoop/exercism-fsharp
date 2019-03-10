27ecce3d7b2345c50a73fe8b796c011d8b8fa560
Michael Cooper <coop87@gmail.com>
2019-03-10 19:23:57 -0400

    Finish AllYourBase and LargestSeriesProduct

---
+++ all-your-base/AllYourBase.fs
@@ -0,0 +1,24 @@
+﻿module AllYourBase
+
+let rebase (digits:int list) inputBase outputBase =
+    let fromDecimal outputBase num =
+        let rec innerFn num outputBase =
+            match num / outputBase, num % outputBase with
+            | 0, r -> [ r ] 
+            | d, r -> r :: innerFn d outputBase
+        if outputBase < 2 then
+            None
+        else
+            Some (innerFn num outputBase |> List.rev)
+    let toDecimal inputBase digits =
+        if digits |> List.exists (fun n -> n < 0 || n >= inputBase) || inputBase < 2 then
+            None
+        else
+            digits
+            |> List.rev
+            |> List.indexed
+            |> List.map (fun (i, n) -> n * pown inputBase i)
+            |> List.sum
+            |> Some
+    let decimal = toDecimal inputBase digits
+    decimal |> Option.bind (fromDecimal outputBase)
---
+++ largest-series-product/LargestSeriesProduct.fs
@@ -0,0 +1,18 @@
+﻿module LargestSeriesProduct
+
+let largestProduct (input:string) seriesLength =
+    if seriesLength = 0 then
+        Some 1
+    elif seriesLength > input.Length || seriesLength < 0 then
+        None
+    elif (String.forall System.Char.IsDigit input) = false then
+        None
+    else
+        input.ToCharArray()
+        |> List.ofArray
+        |> List.map (fun c -> c.ToString())
+        |> List.map System.Int32.Parse
+        |> List.windowed seriesLength
+        |> List.map (fun list -> List.fold (*) 1 list)
+        |> List.max
+        |> Some
