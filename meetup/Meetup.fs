2389bd3b0145990c1c6eda0e7c14b7048f1d7567
Michael Cooper <coop87@gmail.com>
2019-03-09 22:58:37 -0500

    Finish Meetup and TwelveDays

---
+++ meetup/Meetup.fs
@@ -0,0 +1,21 @@
+﻿module Meetup
+
+open System
+
+type Week = First | Second | Third | Fourth | Last | Teenth
+
+let meetup year month week dayOfWeek =
+    let getDates week =
+        let daysInMonth = DateTime.DaysInMonth(year, month)
+        let weekDates = [ 1 .. daysInMonth ] |> List.chunkBySize 7
+        match week with
+        | First -> weekDates.[0]
+        | Second -> weekDates.[1]
+        | Third -> weekDates.[2]
+        | Fourth -> weekDates.[3]
+        | Last -> [ daysInMonth - 6 .. daysInMonth]
+        | Teenth -> [ 13 .. 19]
+
+    getDates week
+    |> List.map (fun day -> DateTime(year, month, day))
+    |> List.find (fun date -> date.DayOfWeek = dayOfWeek)
--- twelve-days/TwelveDays.fs
+++ twelve-days/TwelveDays.fs
@@ -1,3 +1,35 @@
 ﻿module TwelveDays
 
-let recite start stop = failwith "You need to implement this function."
+let recite start stop =
+    let allVerses =
+        [ "first", "a Partridge in a Pear Tree"
+          "second", "two Turtle Doves"
+          "third", "three French Hens"
+          "fourth", "four Calling Birds"
+          "fifth", "five Gold Rings"
+          "sixth", "six Geese-a-Laying"
+          "seventh", "seven Swans-a-Swimming"
+          "eighth", "eight Maids-a-Milking"
+          "ninth", "nine Ladies Dancing"
+          "tenth", "ten Lords-a-Leaping"
+          "eleventh", "eleven Pipers Piping"
+          "twelfth", "twelve Drummers Drumming" ]
+    
+    let getVerse n =
+        let firstPart =
+            let count = fst allVerses.[n - 1]
+            sprintf "On the %s day of Christmas my true love gave to me: " count
+        
+        let secondPart =
+            let items = allVerses.[..n - 1] |> List.map snd
+            match items with
+            | [ ] -> [ ]
+            | [ s ] -> [ s ]
+            | head :: tail ->
+                ("and " + head) :: tail
+            |> List.rev
+            |> String.concat ", "
+        firstPart + secondPart + "."
+    
+    [ for n in start .. stop do
+        yield getVerse n ]
