2a38b76e3674366f9f3bef21175addee4b1888e9
Michael Cooper <coop87@gmail.com>
2019-03-09 19:41:50 -0500

    Finish SecretHandshake, TwelveDays, WordCount

---
+++ secret-handshake/SecretHandshake.fs
@@ -0,0 +1,19 @@
+﻿module SecretHandshake
+
+let commands number =
+    let bitSet b n = n &&& b = b
+    let list =
+        [ 1, "wink"
+          2, "double blink"
+          4, "close your eyes"
+          8, "jump" ]
+        |> List.choose (fun (b, s) ->
+            if number |> bitSet b then
+                Some s
+            else
+                None)
+    if number |> bitSet 16 then
+        list |> List.rev
+    else
+        list
+    
---
+++ word-count/WordCount.fs
@@ -0,0 +1,11 @@
+﻿module WordCount
+
+open System
+
+let countWords (phrase:string) =
+    let isValidChar c = System.Char.IsLetterOrDigit c || c = ',' || c = ' ' || c = '\''
+    let filtered = String.filter isValidChar phrase
+    filtered.ToLower().Split([| ' '; ',' |], StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map (fun w -> w.TrimStart('\'').TrimEnd('\''))
+    |> Array.countBy id
+    |> Map.ofArray
