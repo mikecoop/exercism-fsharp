e7d9762904a827a237b57094448dc9564a0f98dc
Michael Cooper <coop87@gmail.com>
2019-03-15 00:00:34 -0400

    Finish PrimeFactors

---
+++ prime-factors/PrimeFactors.fs
@@ -0,0 +1,12 @@
+﻿module PrimeFactors
+
+let factors (number:int64) : int list =
+    let rec primeFactors factor = function
+    | number when number % factor = 0L ->
+        factor :: (primeFactors factor (number / factor))
+    | number when number > factor ->
+        primeFactors (factor + 1L) number
+    | number when number > 1L ->
+        [ number ]
+    | _ -> [ ]
+    primeFactors 2L number |> List.map int32
