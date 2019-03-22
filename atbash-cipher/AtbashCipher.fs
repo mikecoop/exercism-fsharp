309fe0312daeaae27e39c0ce7767cd7bc634037b
Mike Cooper <mikecoop@users.noreply.github.com>
2019-03-22 19:53:45 -0400

    Finish AtbashCipher

---
+++ atbash-cipher/AtbashCipher.fs
@@ -0,0 +1,23 @@
+﻿module AtbashCipher
+
+let alphabet = [ 'a'..'z' ]
+
+let pairMap = List.zip alphabet (List.rev alphabet) |> Map.ofList
+
+let swap (str:string) =
+    str.ToLower()
+    |> String.map (fun c ->
+        match pairMap.TryGetValue c with
+        | true, swapped -> swapped
+        | false, _ -> c)
+
+let filterAlphaNumeric str = String.filter (System.Char.IsLetterOrDigit) str
+
+let chunkByFive (str:string) =
+    Seq.chunkBySize 5 str
+    |> Seq.map (fun arr -> System.String(arr))
+    |> String.concat " "
+
+let encode = swap >> filterAlphaNumeric >> chunkByFive
+
+let decode = filterAlphaNumeric >> swap
