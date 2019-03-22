195e421ae6f15d5acfd1936b5ec1cd8d5776d72e
Michael Cooper <coop87@gmail.com>
2019-03-22 18:00:43 -0400

    Working on ScaleGenerator.

---
+++ scale-generator/ScaleGenerator.fs
@@ -0,0 +1,20 @@
+﻿module ScaleGenerator
+
+let sharps = [ "A"; "A#"; "B"; "C"; "C#"; "D"; "D#"; "E"; "F"; "F#"; "G"; "G#" ]
+
+let flats = [ "A"; "Bb"; "B"; "C"; "Db"; "D"; "Eb"; "E"; "F"; "Gb"; "G"; "Ab" ]
+
+let usesSharps = [ "C"; "G"; "D"; "A"; "E"; "B"; "F#"; "e"; "b"; "f#"; "c#"; "g#"; "d#" ] |> Set.ofList
+
+let chromatic (tonic:string) =
+    let getNotes tonic =
+        if usesSharps |> Set.exists ((=) tonic) then
+            sharps @ sharps
+        else
+            flats @ flats
+    let notes = getNotes tonic
+    match List.tryFindIndex ((=) tonic) notes with
+    | None -> [ ]
+    | Some index -> notes.[index..] |> List.take 12
+
+let interval intervals tonic = failwith "You need to implement this function."
