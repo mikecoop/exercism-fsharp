25821d1bee78924f5cf09bc9d833dc0d1f5d4277
Michael Cooper <coop87@gmail.com>
2019-03-24 16:43:50 -0400

    CircularBuffer first 11 test passing.

---
+++ circular-buffer/CircularBuffer.fs
@@ -0,0 +1,30 @@
+﻿module CircularBuffer
+
+type CircularBuffer<'a> =
+    { Size: int
+      Items: 'a list }
+
+let bufferIsFull buffer =
+    buffer.Size = List.length buffer.Items
+
+let mkCircularBuffer size =
+    { Size = size; Items = List.empty }
+
+let clear buffer =
+    { buffer with Items = List.empty }
+
+let write value buffer =
+    if bufferIsFull buffer then
+        failwith "Buffer is full"
+    else
+        { buffer with Items = value :: buffer.Items}
+
+let forceWrite value buffer =
+    write value buffer
+
+let read buffer =
+    match buffer.Items |> List.rev with
+    | [ ] ->
+        failwith "Buffer is empty"
+    | head :: rest ->
+        head, { buffer with Items = rest |> List.rev }
