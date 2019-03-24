b062c5213041f77612220d9e0cccd8664878974e
Michael Cooper <coop87@gmail.com>
2019-03-24 09:13:10 -0400

    Finish CryptoSquare

---
+++ crypto-square/CryptoSquare.fs
@@ -0,0 +1,30 @@
+﻿module CryptoSquare
+
+open System
+
+let ciphertext input =
+    let normalize input =
+        input
+        |> String.filter Char.IsLetterOrDigit
+        |> String.map Char.ToLower
+
+    let applyCipher normalized =
+        let columns = normalized |> String.length |> float |> sqrt |> ceil |> int
+        normalized
+        |> Seq.chunkBySize columns
+        |> Seq.transpose
+        |> Seq.map (Seq.toArray >> System.String)
+
+    let appendSpaces desiredLength str =
+        if String.length str < desiredLength then
+            str + String.replicate (desiredLength - String.length str) " "
+        else
+            str
+
+    if input = "" then
+        ""
+    else
+        let words = normalize input |> applyCipher
+        words
+        |> Seq.map (appendSpaces (Seq.head words).Length)
+        |> String.concat " "
