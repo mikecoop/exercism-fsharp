c44453586df671312633aa37ec3115562ae45b1a
Michael Cooper <coop87@gmail.com>
2019-03-25 19:18:19 -0400

    Finish BankAccount

---
+++ bank-account/BankAccount.fs
@@ -0,0 +1,32 @@
+﻿module BankAccount
+
+type OpenAccount = { mutable Balance: decimal }
+
+type Account =
+    | Closed
+    | Open of OpenAccount
+
+let mkBankAccount() = Closed
+
+let openAccount account =
+    match account with
+    | Closed -> Open { Balance = 0.0m }
+    | Open _ -> failwith "Account is already open"
+
+let closeAccount account =
+    match account with
+    | Open _ -> Closed
+    | Closed -> failwith "Account is already closed"
+
+let getBalance account =
+    match account with
+    | Open openAccount -> Some openAccount.Balance
+    | Closed -> None
+
+let updateBalance change account =
+    match account with
+    | Open openAccount ->
+        lock (openAccount) (fun _ ->
+            openAccount.Balance <- openAccount.Balance + change
+            Open openAccount)
+    | Closed -> failwith "Account is closed"
