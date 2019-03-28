95d95dbfada0f967a09e5e480a7091cf3bc43bf4
Michael Cooper <coop87@gmail.com>
2019-03-27 22:11:29 -0400

    Finish RunLengthEncoding

--- run-length-encoding/RunLengthEncoding.fs
+++ run-length-encoding/RunLengthEncoding.fs
@@ -1,5 +1,40 @@
 ﻿module RunLengthEncoding
 
-let encode input = failwith "You need to implement this function."
+open System
+open System.Text.RegularExpressions
 
-let decode input = failwith "You need to implement this function."
+let encode (input:string) : string =
+    let folder char counts =
+        match counts with
+        | [ ] -> [ (1, char) ]
+        | (count, lastChar) :: rest ->
+            if char = lastChar then
+                (count + 1, lastChar) :: rest
+            else
+                (1, char) :: counts
+
+    let countToString (count, char) =
+        if count = 1 then
+            char.ToString()
+        else
+            sprintf "%i%c" count char
+
+    Seq.foldBack folder input [ ]
+    |> List.map countToString
+    |> String.concat ""
+
+let decode (input:string) =
+    let regex = Regex ("(\d+)?(\D)")
+    let matches = regex.Matches (input) |> Seq.cast<Match>
+    let countPairs =
+        matches
+        |> Seq.toList
+        |> List.map (fun m -> m.Groups.[1].Value, m.Groups.[2].Value)
+    let pairToString (count, char) =
+        match count with
+        | "" -> char
+        | _ -> String.replicate (Int32.Parse count) char
+
+    countPairs
+    |> List.map pairToString
+    |> String.concat ""
