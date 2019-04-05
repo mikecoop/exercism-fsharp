188ed34edc9f346143171da0f570acac7f80ee3b
Michael Cooper <coop87@gmail.com>
2019-04-04 23:47:37 -0400

    Working on WordSearch

--- word-search/WordSearch.fs
+++ word-search/WordSearch.fs
@@ -1,18 +1,13 @@
 ﻿module WordSearch
 
 let tryFindIndex (word:string) (row:string) =
-    match row.IndexOf (word.[0]) with
-    | -1 -> None
-    | index ->
-        let afterMatch = row.[index..]
-        let beforeMatch = row.[..index]
-        let wordRev = System.String (Seq.rev word |> Seq.toArray)
-        if afterMatch.Length >= word.Length && afterMatch.[.. word.Length - 1] = word then
-            Some (index, index + word.Length - 1)
-        elif beforeMatch.Length >= word.Length && beforeMatch.[index - (word.Length - 1) ..] = wordRev then
-            Some (index, word.Length - 1 - index)
-        else
-            None
+    match row.IndexOf (word) with
+    | -1 ->
+        let wordReversed = System.String (Seq.rev word |> Seq.toArray)
+        match row.IndexOf (wordReversed) with
+        | -1 -> None
+        | index -> Some (index + word.Length - 1, index)
+    | index -> Some (index, index + word.Length - 1)
 
 let tryFindInRows (word:string) (rows:string list) =
     let matches =
@@ -28,8 +23,26 @@ let tryFindInRows (word:string) (rows:string list) =
     | [ ] -> None
     | first :: _ -> Some first
 
+let tryFindInColumns (word:string) (rows:string list) =
+    let columns =
+        rows
+        |> List.map (fun str -> str.ToCharArray() |> List.ofArray)
+        |> List.transpose
+        |> List.map (List.toArray >> System.String)
+    match (tryFindInRows word columns) with
+    | Some ((y1, x1), (y2, x2)) -> Some ((x1, y1), (x2, y2))
+    | None -> None
+
+let findMatch grid word =
+    match (grid |> tryFindInRows word) with
+    | Some location ->
+        Some location
+    | None ->
+        match (grid |> tryFindInColumns word) with
+        | Some location -> Some location
+        | None -> None
+
 let search grid (wordsToSearchFor:string list) =
     wordsToSearchFor
-    |> List.map
-        (fun word -> word, grid |> tryFindInRows word)
+    |> List.map (fun word -> word, findMatch grid word)
     |> Map.ofList
