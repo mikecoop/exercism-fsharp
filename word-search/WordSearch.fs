e8a7178c86a618ac9855db9b4853f3e1f1befdac
Michael Cooper <coop87@gmail.com>
2019-04-07 21:43:14 -0400

    Working on WordSearch

--- word-search/WordSearch.fs
+++ word-search/WordSearch.fs
@@ -1,6 +1,6 @@
 ﻿module WordSearch
 
-let tryFindIndex (word:string) (row:string) =
+let findInRow (word:string) (row:string) =
     match row.IndexOf (word) with
     | -1 ->
         let wordReversed = System.String (Seq.rev word |> Seq.toArray)
@@ -9,13 +9,13 @@ let tryFindIndex (word:string) (row:string) =
         | index -> Some (index + word.Length - 1, index)
     | index -> Some (index, index + word.Length - 1)
 
-let tryFindInRows (word:string) (rows:string list) =
+let findInRows (word:string) (rows:string list) =
     let matches =
         rows
         |> List.indexed
         |> List.choose
             (fun (row, rowStr) ->
-                let columns = rowStr |> tryFindIndex word
+                let columns = rowStr |> findInRow word
                 match columns with
                 | None -> None
                 | Some (y1, y2) -> Some ((y1 + 1, row + 1), (y2 + 1, row + 1)))
@@ -23,22 +23,22 @@ let tryFindInRows (word:string) (rows:string list) =
     | [ ] -> None
     | first :: _ -> Some first
 
-let tryFindInColumns (word:string) (rows:string list) =
+let findInColumns (word:string) (rows:string list) =
     let columns =
         rows
         |> List.map (fun str -> str.ToCharArray() |> List.ofArray)
         |> List.transpose
         |> List.map (List.toArray >> System.String)
-    match (tryFindInRows word columns) with
+    match (findInRows word columns) with
     | Some ((y1, x1), (y2, x2)) -> Some ((x1, y1), (x2, y2))
     | None -> None
 
 let findMatch grid word =
-    match (grid |> tryFindInRows word) with
+    match (grid |> findInRows word) with
     | Some location ->
         Some location
     | None ->
-        match (grid |> tryFindInColumns word) with
+        match (grid |> findInColumns word) with
         | Some location -> Some location
         | None -> None
 
