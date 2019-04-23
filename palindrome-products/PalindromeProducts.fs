575246aa9b22d16875d1b7e3d2888fc4746ac077
Mike Cooper <mikecoop@users.noreply.github.com>
2019-04-22 21:00:24 -0400

    Finish PalindromeProducts.

--- palindrome-products/PalindromeProducts.fs
+++ palindrome-products/PalindromeProducts.fs
@@ -1,5 +1,30 @@
 ﻿module PalindromeProducts
 
-let largest minFactor maxFactor = failwith "You need to implement this function."
+let isPalindrome n =
+    let rec rev acc = function
+        | 0 -> acc
+        | x -> rev (acc * 10 + (x % 10)) (x / 10)
+    n = rev 0 n
 
-let smallest minFactor maxFactor = failwith "You need to implement this function."
+let palindromeProductFactors minFactor maxFactor =
+    [ for x in [ minFactor .. maxFactor ] do
+        for y in [ x .. maxFactor ] do
+            let product = x * y
+            if isPalindrome product
+            then yield (product, (x, y)) ]
+
+let selectFactor minFactor maxFactor fSelector =
+    match palindromeProductFactors minFactor maxFactor with
+    | [ ] -> None
+    | factors ->
+        factors
+        |> List.groupBy fst
+        |> List.map (fun (factor, values) -> factor, values |> List.map snd)
+        |> fSelector fst
+        |> Some
+
+let largest minFactor maxFactor =
+    selectFactor minFactor maxFactor List.maxBy
+
+let smallest minFactor maxFactor =
+    selectFactor minFactor maxFactor List.minBy
