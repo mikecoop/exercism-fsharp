6b906823d88c3e6dbcfdb1cc94bc0c8266749ddb
Mike Cooper <mikecoop@users.noreply.github.com>
2019-04-23 00:12:33 -0400

    Start RationalNumbers.

--- rational-numbers/RationalNumbers.fs
+++ rational-numbers/RationalNumbers.fs
@@ -1,19 +1,50 @@
 ﻿module RationalNumbers
 
-let create numerator denominator = failwith "You need to implement this function."
+open System
 
-let add r1 r2 = failwith "You need to implement this function."
+type RationalNumber = { Numerator: int; Denominator: int }
 
-let sub r1 r2 = failwith "You need to implement this function."
+let negate n = { n with Numerator = -n.Numerator }
 
-let mul r1 r2 = failwith "You need to implement this function."
+let fixSign n =
+    if n.Denominator < 0 then
+        { Numerator = -n.Numerator; Denominator = -n.Denominator }
+    else
+        n
 
-let div r1 r2 = failwith "You need to implement this function."
+let invert n =
+    { Numerator = n.Denominator; Denominator = n.Numerator } |> fixSign
 
-let abs r = failwith "You need to implement this function."
+let create numerator denominator = { Numerator = numerator; Denominator = denominator }
 
-let exprational n r = failwith "You need to implement this function."
+let add r1 r2 =
+    let left, right = r1.Numerator * r2.Denominator, r2.Numerator * r1.Denominator
+    if left = -right then
+        { Numerator = 0; Denominator = 1 }
+    else
+        { Numerator = left + right; Denominator = r1.Denominator * r2.Denominator }
 
-let expreal r n = failwith "You need to implement this function."
+let sub r1 r2 = add r1 (negate r2)
 
-let reduce r = failwith "You need to implement this function."
+let reduce r =
+    if r.Numerator % r.Denominator = 0 then
+        { Numerator = r.Numerator / r.Denominator
+          Denominator = 1 }
+    elif r.Denominator % r.Numerator = 0 then
+        { Numerator = 1
+          Denominator = r.Denominator / r.Numerator }
+    else r
+    |> fixSign
+
+let mul r1 r2 =
+    { Numerator = r1.Numerator * r2.Numerator
+      Denominator = r1.Denominator * r2.Denominator }
+    |> reduce
+
+let div r1 r2 = mul r1 (invert r2)
+
+let abs r = { Numerator = abs r.Numerator; Denominator = r.Denominator }
+
+let exprational n r = { Numerator = pown r.Numerator n; Denominator = pown r.Denominator n }
+
+let expreal r n = Math.Pow (float n, (float r.Numerator) / (float r.Denominator))
