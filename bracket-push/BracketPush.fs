11b38fec776ef9b836b77d1af5756716e0651821
Mike Cooper <mikecoop@users.noreply.github.com>
2019-04-28 10:06:00 -0400

    Finish BracketPush

---
+++ bracket-push/BracketPush.fs
@@ -0,0 +1,26 @@
+﻿module BracketPush
+
+let bracketPairs =
+    [ ('[', ']')
+      ('{', '}')
+      ('(', ')') ]
+
+let pushPop list char =
+    match bracketPairs |> List.tryFind (fun (first, _) -> first = char) with
+    | Some pair ->
+        fst pair :: list
+    | None ->
+        match bracketPairs |> List.tryFind (fun (_, second) -> second = char) with
+        | None -> list
+        | Some pair ->
+            match list with
+            | next :: rest ->
+                if fst pair = next then
+                    rest
+                else
+                    snd pair :: list
+            | _ -> list
+
+let isPaired (input:string) =
+    let result = input.ToCharArray() |> List.ofArray |> Seq.fold pushPop [ ]
+    List.isEmpty result
