cdf653df69ceead72740b17f7d8a5c76671e2715
Mike Cooper <mikecoop@users.noreply.github.com>
2019-04-29 22:46:09 -0400

    Start Change

---
+++ change/Change.fs
@@ -0,0 +1,19 @@
+﻿module Change
+
+let rec change acc coins target =
+    match coins with
+    | [ ] -> acc
+    | coin :: rest ->
+        let remainder = target - coin
+        if remainder = 0 then
+            target :: acc
+        elif remainder > 0 then
+            change (target :: acc) rest target
+        else
+            [ ]
+
+let findFewestCoins coins target =
+    let bigToSmall = coins |> Seq.rev
+    Some [ 25 ]
+
+change [ ] (List.rev [1; 5; 10; 25; 100]) 25
